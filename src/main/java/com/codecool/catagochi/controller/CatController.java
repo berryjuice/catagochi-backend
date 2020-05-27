83eb7fab01a45d279cfedaff1ca160277d77fbe5
Katalin Nemeth <zeoramail@gmail.com>
2020-05-27 14:19:28 +0200

    my-cats/{cat-name} displays details of the cat

--- src/main/java/com/codecool/catagochi/controller/CatController.java
+++ src/main/java/com/codecool/catagochi/controller/CatController.java
@@ -4,6 +4,7 @@ import com.codecool.catagochi.model.Cat;
 import com.codecool.catagochi.data.CatStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +18,13 @@ public class CatController {
     @GetMapping("/my-cats")
     public List<Cat> catList() {
         return catStorage.getAllCats();
+    }
+
+    @GetMapping("/my-cats/{name}")
+    public Cat catDetails(@PathVariable("name") String name) throws Exception {
+        return catStorage.getAllCats().stream()
+                .filter(c -> c.getName().equals(name))
+                .findFirst()
+                .orElseThrow(()->new Exception("Cat not found with name: "+name));
     }
 }
