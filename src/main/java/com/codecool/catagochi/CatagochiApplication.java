04e82f11faacb89ad9b39fd3d3c3a87789bdf110
Katalin Nemeth <zeoramail@gmail.com>
2020-05-27 10:28:20 +0200

    project initialized

---
+++ src/main/java/com/codecool/catagochi/CatagochiApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.catagochi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatagochiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatagochiApplication.class, args);
+    }
+
+}
