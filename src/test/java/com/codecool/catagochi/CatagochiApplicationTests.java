f181d59e1dd840b28b4ebfb13eab411f8f41f66d
Katalin Nemeth <zeoramail@gmail.com>
2020-06-03 11:19:52 +0200

    gitignore modified

---
+++ src/test/java/com/codecool/catagochi/CatagochiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.catagochi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatagochiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
