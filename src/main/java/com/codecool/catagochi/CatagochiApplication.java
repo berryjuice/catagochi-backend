37ea9dbc5970fe88d373fbfa6f16bc206f33df93
Katalin Nemeth <zeoramail@gmail.com>
2020-06-10 19:58:19 +0200

    proper initials list of cats added

--- src/main/java/com/codecool/catagochi/CatagochiApplication.java
+++ src/main/java/com/codecool/catagochi/CatagochiApplication.java
@@ -15,6 +15,10 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 @EnableScheduling
 public class CatagochiApplication {
@@ -38,31 +42,43 @@ public class CatagochiApplication {
     @Profile("production")
     public CommandLineRunner init() {
         return args -> {
-            Cat cat = Cat.builder()
+            List<Cat> cats = new ArrayList<>();
+            cats.add(Cat.builder()
                     .name("Jonesy")
                     .gender(Gender.MALE)
                     .age(Age.ADULT)
-                    .img("image")
-                    .breed(Breed.PERSIAN)
-                    .build();
-            catRepository.save(cat);
+                    .img("https://d2ph5fj80uercy.cloudfront.net/04/cat2972.jpg")
+                    .build());
 
-            Cat cat2 = Cat.builder()
-                    .name("Maszat")
+            cats.add(Cat.builder()
+                    .name("Schrödinger")
+                    .gender(Gender.MALE)
+                    .age(Age.YOUNG)
+                    .img("https://d2ph5fj80uercy.cloudfront.net/06/cat1996.jpg")
+                    .build());
+
+            cats.add(Cat.builder()
+                    .name("Ópamacs")
                     .gender(Gender.MALE)
                     .age(Age.ADULT)
-                    .img("image")
-                    .build();
-            catRepository.save(cat2);
+                    .img("https://d2ph5fj80uercy.cloudfront.net/06/cat2630.jpg")
+                    .build());
 
-            Cat cat3 = Cat.builder()
-                    .name("Guri")
-                    .gender(Gender.MALE)
+            cats.add(Cat.builder()
+                    .name("Guriga")
+                    .gender(Gender.FEMALE)
                     .age(Age.ADULT)
-                    .breed(Breed.RUSSIANBLUE)
-                    .img("image")
-                    .build();
-            catRepository.save(cat3);
+                    .img("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDUBhnLKerrtsNVx1l-QiaPrnJfG5zNFY-O5F_Xc9XNNJgukrj&usqp=CAU")
+                    .build());
+
+            cats.add(Cat.builder()
+                    .name("Lukrécia")
+                    .gender(Gender.FEMALE)
+                    .age(Age.ADULT)
+                    .img("https://www.stylist.co.uk/images/app/uploads/2018/10/11175734/kazuky-akayashi-703909-unsplash-256x256.jpg?w=256&h=256&fit=max&auto=format%2Ccompress")
+                    .build());
+
+            catRepository.saveAll(cats);
         };
     }
 }
